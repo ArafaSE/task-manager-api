a04b1b3fb9301700bf97d67f9d54acf4dbb03aa7
ArafaSE <mohamed.arafa.swt@gmail.com>
2020-09-07 21:21:38 +0200

    Create user test cases

---
+++ tests/user.test.js
@@ -0,0 +1,137 @@
+const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const app = require('../src/app')
+const User = require('../src/models/user')
+const { resource } = require('../src/app')
+const { findById } = require('../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    _id: userOneId,
+    name: 'Mike',
+    email: 'mike@gmail.com',
+    password: 'mike1234',
+    tokens:[{
+        token: jwt.sign({ _id: userOneId}, process.env.JWT_SECRET)
+    }]
+}
+
+beforeEach( async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('Should signup a new user', async () => {
+    const response = await request(app).post('/users').send({
+        name: 'Mohamed',
+        email: 'mohamed@gmail.com',
+        password: 'mohamed1234'
+    }).expect(201)
+
+    // Assert that user is saved in database correctly 
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+    
+    // Assertion about the response body
+    expect(response.body).toMatchObject({
+        user:{
+            name: 'Mohamed',
+            email: 'mohamed@gmail.com'
+        },
+        token: user.tokens[0].token
+    })
+
+    // Assertion to make sure the password saved as hashed passwrod in database
+    expect(user.password).not.toBe('mohamed1234')
+})
+
+test('Should login existing user', async () => {
+    const response = await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+
+    const user = await User.findById(userOneId)
+
+    // Assert that the user get the second token after login
+    expect(response.body.token).toBe(user.tokens[1].token)
+})
+
+test('Should not login non existent user', async ()=> {
+    await request(app)
+    .post('/users/login')
+    .send({
+        email: userOne.email,
+        password: 'hamada1234'
+    })
+    .expect(400)
+})
+
+test('Should get profile for user', async () => {
+    await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
+
+
+test('Should delete account for user', async () => {
+    await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+
+    // Assertion to make sure the user is deleted from database
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
+})
+
+test('Should not delete account for unauthorized user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('Should upload avatar image', async() => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/__mocks__/fixtures/profile-pic.jpg')
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Hamada'
+        })
+        .expect(200)
+    // Assert that user updated in the DB correctly
+    const user = await User.findById(userOneId) 
+    expect(user.name).toBe('Hamada')
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'Cairo'
+        })
+        .expect(400)
+})
